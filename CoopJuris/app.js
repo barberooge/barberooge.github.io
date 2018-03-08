d76dbb37e8ca672c9e7f415b771bbceb9d8b73de
Mathieu Mo <mathieumonnin@me.com>
2018-03-08 22:15:08 +0100

    Test Listen Event JS / CJ

--- CoopJuris/app.js
+++ CoopJuris/app.js
@@ -1,9 +1,11 @@
 var positionSmall = 0;
-$(document).scroll(function () {
-    positionSmall = $(this).scrollTop();
-    if (positionSmall > 1) {
-        $(".navbar").css('background-color', '#2C3E52');
-    } else {
-        $(".navbar").css('background-color', 'transparent');
-    }
+$(document).ready(function () {
+    $(document).scroll(function () {
+        positionSmall = $(this).scrollTop();
+        if (positionSmall > 1) {
+            $(".navbar").css('background-color', '#2C3E52');
+        } else {
+            $(".navbar").css('background-color', 'transparent');
+        }
+    });
 });
